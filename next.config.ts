432147b6eed505cb5206321e653fafed30725a5b
yatindeveloper2011 <yatindeveloper2011@gmail.com>
2026-08-11 15:05:52 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
       { protocol: "https", hostname: "images.unsplash.com" },
